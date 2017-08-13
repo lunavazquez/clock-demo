473308e330174fa9d021e8e1cafb1a8426686093
Adriana Vazquez <luna.vazquezv@gmail.com>
2017-08-13 18:33:25 -0500

    Reloj terminado
    Reloj hecho con HTML5, CSS y JS vanilla.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
-console.log('Hola mundo');
+var clockDiv = document.getElementById('clock');
+
+var CalculateDate = function() {
+    // get system date
+    var date = new Date();
+    // calculate hours
+    var hours = date.getHours();
+    hours = hours < 10 ? '0' + hours : hours;
+    // calculate minutes
+    var minutes = date.getMinutes();
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    // calculate seconds
+    var seconds = date.getSeconds();
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    // insert date string into div#clock
+    clockDiv.innerHTML = hours + ':' + minutes + ':' + seconds;
+}
+CalculateDate();
+
+setInterval(CalculateDate, 1000);
